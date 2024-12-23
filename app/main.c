22bc8d8499e98fea9207030ff3bf737bf5052b92
AcePocko <acepocko@gmail.com>
2024-12-23 20:40:39 +0600

    test6

--- app/main.c
+++ app/main.c
@@ -216,49 +216,41 @@ int tokenize(char *input_str, char **args) {
         token_in = true;
       }
         if (input_str[input_idx] == '\\' && !dwait && quote_in) {
-                    // Treat backslash literally
-                    
-                    temp[token_idx] = '\\';
-                    token_idx++;
-                    input_idx++;
-                }else if(dwait && input_str[input_idx] == '\\'){
-                  char nextChar = input_str[input_idx+1];
-                  switch (nextChar)
-                  {
-                  case '\"':
-                    temp[token_idx] = '\"';
-        
-                    token_idx++;
-                    input_idx++; 
-                    break;
-                  case '\\':
-                    temp[token_idx] = '\\';
-                    token_idx++;
-                    input_idx++; 
-                    break;
-                  case 'n':
-                    temp[token_idx] = 'n';
-                    token_idx++;
-                    input_idx++; 
-                    break;
-                  case '$':
-                    temp[token_idx] = '$';
-                    token_idx++;
-                    input_idx++;    
-                    break;
-                  default:
-                    temp[token_idx] = '\\';
-                    token_idx++;
-                    input_idx++; 
-                    break;
-                  }
-                  
-                  
-                }else{
-                  temp[token_idx] = input_str[input_idx];
-                  token_idx++;
-                }
-      
+          // Treat backslash literally
+          temp[token_idx] = '\\';
+          token_idx++;
+          input_idx++;
+        }else if(dwait && input_str[input_idx] == '\\'){
+          char nextChar = input_str[input_idx+1];
+          switch (nextChar)
+          {
+            case '\"':
+              temp[token_idx] = '\"';
+              token_idx++;
+              break;
+            case '\\':
+              temp[token_idx] = '\\';
+              token_idx++;
+              break;
+            case 'n':
+              temp[token_idx] = 'n';
+              token_idx++;
+              break;
+            case '$':
+              temp[token_idx] = '$';
+              token_idx++;
+              break;
+            default:
+              temp[token_idx] = '\\';
+              token_idx++;
+              break;
+            }
+            input_idx++;
+                        
+          }else{
+            temp[token_idx] = input_str[input_idx];
+            token_idx++;
+          }
     } 
   }
   return token_num;
